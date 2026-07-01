094b2b1e50f44173a86f205d2c2ad15d27f09be5
jakerothacker <jakerothacker@gmail.com>
2026-07-01 15:57:21 -0400

    documentation for problem two

--- problem_two.py
+++ problem_two.py
@@ -1,5 +1,13 @@
 #sum of all even Fibonacci numbers below 4 million
 def sum_of_even_fibonacci_below_n(n):
+    """summs even fibonacci numbers up to n-1
+
+    Args:
+        n (int): The upper limit (exclusive) for the Fibonacci numbers to consider
+
+    Returns:
+        int: The sum of all even Fibonacci numbers below n
+    """
     sum = 0 
     count = 1
     last_count = 0
