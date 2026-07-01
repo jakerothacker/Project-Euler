561b1ab647da32770752cce086bf10010c3eedb2
Jake Rothacker <jakerothacker@gmail.com>
2026-07-01 15:27:12 -0400

    problem one: multiples of 3 and 5

---
+++ problem_one.py
@@ -0,0 +1,14 @@
+#sum of all multiples of 3 and 5 below 1000
+
+def sum_of_multiples_of_3_and_5_below_n(n):
+    sum = 0
+    count = 0
+    for i in range(n):
+        if count % 3 == 0:
+            sum += count
+        elif count % 5 == 0:
+            sum += count
+        count += 1
+    return (print(sum))
+
+sum_of_multiples_of_3_and_5_below_n(1000)
