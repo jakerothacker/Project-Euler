1cd1e59cf5629c7b50f73e4b4094c5bd2f76fe58
jakerothacker <jakerothacker@gmail.com>
2026-07-02 00:31:18 -0400

    current work on problem four (not done)

--- problem_four.py
+++ problem_four.py
@@ -1,6 +1,9 @@
 # largest palindrome that is the product of two 3-digit numbers
+import math
 
-def check_palindrome(n):
+
+
+def is_palindrome(n):
     """
     This function checks if a given number is a palindrome.
 
@@ -11,4 +14,15 @@ def check_palindrome(n):
     bool: True if the number is a palindrome, False otherwise.
     """
     return str(n) == str(n)[::-1]
+             
 
+def largest_palindrome_product_2_factor(n):
+    guess_1 = 10**n -1
+    guess_2 = guess_1
+    while True:
+        if is_palindrome(guess_1 * guess_2):
+            return(guess_1*guess_2)
+        else:
+             guess_2 -=1
+
+print(largest_palindrome_product_2_factor(3))
