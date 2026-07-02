16123cefc0e715ec6d891fcd92180f569310c6fb
jakerothacker <jakerothacker@gmail.com>
2026-07-02 00:20:57 -0400

    optimised is_prime function (?)

--- prime_function_stroage.py
+++ prime_function_stroage.py
@@ -9,7 +9,12 @@ def is_prime(n):
     """
     if n <= 1:
         return False
-    for i in range(2, int(n**0.5) + 1):
+    elif n <= 3:
+        return True
+    elif n % 2 == 0:
+        return False
+    
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
