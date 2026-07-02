ae5522fa678b38ccbcb223725ac2a0660da3b0eb
jakerothacker <jakerothacker@gmail.com>
2026-07-02 00:07:49 -0400

    problem six complete

---
+++ problem_six.py
@@ -0,0 +1,21 @@
+#sum square difference
+
+def sum_square(n):
+    """Finds the sum of the squares of the first n natural numbers"""
+    sum_of_squares = 0
+    for i in range(1,n+1):
+        sum_of_squares += i**2
+    return sum_of_squares
+
+def square_sum(n):
+    """Finds the square of the sum of the first n natural numbers"""
+    square_of_sum = 0
+    for i in range(1,n+1):
+        square_of_sum += i
+    return square_of_sum**2
+
+def sum_square_difference(n):
+    """Finds the difference between the sum of the squares and the square of the sum of the first n natural numbers"""
+    return square_sum(n) - sum_square(n)
+
+print(sum_square_difference(100))
