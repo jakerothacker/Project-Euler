4622ad995fbd83cbe395d929979eaf05225bfa02
jakerothacker <jakerothacker@gmail.com>
2026-07-03 22:45:15 -0400

    Problem 9 is done

---
+++ problem_nine.py
@@ -0,0 +1,48 @@
+#Pythagorian triplet when a+b+c = 1000
+import math
+
+
+def is_right_triangle(a,b,c):
+    """
+    checks if three sides makes a right triangle
+
+    Args:
+        a (int): 0<a<=b
+        b (int): a<=b<c
+        c (int): b<c<inf
+    """
+    small_sides = a**2 + b**2
+    if small_sides == c**2:
+        return True
+    else:
+        return False
+def check_pythagorian_triplet(n):
+    c_start= math.ceil(n/2)
+    b_start= n//2 -1
+    a_start=1
+
+    a=a_start
+    b=b_start
+    c=c_start
+
+    while a_start<b_start:
+        a=a_start
+        b=b_start
+        c=c_start
+        while a<=b:
+            if a+b+c!=1000:
+                return print("something went wrong")
+            if is_right_triangle(a,b,c):
+                return print(f"{a}, {b}, {c}")
+            
+                
+            else:
+                a += 1
+                b -= 1
+        a_start += 1
+        c_start -= 1
+
+    return print("none found")
+
+if __name__ == "__main__":
+    check_pythagorian_triplet(1000) 
