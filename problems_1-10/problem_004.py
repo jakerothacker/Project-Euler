05861e093fddb620b2cf49016ecc1298ded549f8
jakerothacker <jakerothacker@gmail.com>
2026-08-01 17:13:26 -0400

    problem 4 done

--- problems_1-10/problem_004.py
+++ problems_1-10/problem_004.py
@@ -17,12 +17,23 @@ def is_palindrome(n):
              
 
 def largest_palindrome_product_2_factor(n):
-    guess_1 = 10**n -1
-    guess_2 = guess_1
-    while True:
-        if is_palindrome(guess_1 * guess_2):
-            return(guess_1*guess_2)
-        else:
-             guess_2 -=1
+    """
+    This gives the largest palindrome that is the product of two n digit numbers
+    """
+    guess = 10**(2*n) -1
+    while len(str(guess))>n:
+        if is_palindrome(guess):
+            factor_1 = 10**n -1
+            while factor_1 >= 10**(n-1):
+                factor_2 = guess / factor_1
+                if  factor_2 % 1 == 0 and len(str(int(factor_2))) == n:
+                    return print(guess)
+                    
+                else:
+                    factor_1 -= 1
 
-print(largest_palindrome_product_2_factor(3))
+        
+        guess -= 1
+    return print("end")
+
+largest_palindrome_product_2_factor(4)
