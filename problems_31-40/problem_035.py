18ac5060ee568bad5b00a18bf7aaa7914f78d136
jakerothacker <jakerothacker@gmail.com>
2026-09-04 10:56:14 -0400

    problem 35 done

---
+++ problems_31-40/problem_035.py
@@ -0,0 +1,70 @@
+# How many circular primes are there below one million?
+import math
+
+def main(max_num):
+    num = 1
+    circular_primes = []
+    while num <= max_num:
+        if is_circular_prime(num):
+            circular_primes.append(num)
+        num += 1
+    return circular_primes
+
+
+
+def is_circular_prime(n):
+    if not is_prime(n):
+        return False
+    if len(str(n)) == 1:
+        return True
+    else:
+        for i in range(len(str(n))):
+            if not is_prime(int(str(n)[i:]+str(n)[:i])):
+                return False
+    return True
+            
+def is_prime(n):
+    """Checks if an int is prime
+
+    Args:
+        n (int): The number to check
+
+    Returns:
+        bool: True if the number is prime, False otherwise
+    """
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0:
+        return False
+    
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def next_prime(n):
+    """Finds the next highest prime number
+
+    Args:
+        n (int): any positive integer.
+
+    Needs: import math
+    """
+    if n<2:
+        return(2)
+    if n==2:
+        return (3)
+    possible_prime = n + 1
+    while True:
+        for i in range (2,math.ceil(possible_prime**0.5)+1):
+            if possible_prime % i == 0:
+                possible_prime += 1 
+                break
+            elif i==math.ceil(possible_prime**0.5):
+                return(possible_prime)
+
+
+primes = main(1000000)
+print (len(primes))
